21faac90230dcee1a09d088c4ab68ebad3bcf335
Alex Fallenstedt <a.fallenstedt@gmail.com>
2021-11-06 22:11:42 -0700

    initial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+ cardano-node run \
+   --topology ./testnet-topology.json \
+   --database-path ./db \
+   --socket-path ./db/node.socket \
+   --host-addr 0.0.0.0 \
+   --port 1337 \
+   --config ./testnet-config.json
